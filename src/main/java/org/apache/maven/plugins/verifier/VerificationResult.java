dde9da6da7ea1c0b11049a5d419ec6a61d7475a4
Gerd Aschemann <gerd@aschemann.net>
2025-01-12 14:49:39 +0100

    Clean up code with `spotless:apply`
    which leads to many changes (
    Cf. support-and-care/maven-support-and-care#77)

--- src/main/java/org/apache/maven/plugins/verifier/VerificationResult.java
+++ src/main/java/org/apache/maven/plugins/verifier/VerificationResult.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugins.verifier;
 
 /*
@@ -25,10 +43,9 @@ import java.util.List;
 import org.apache.maven.plugins.verifier.model.File;
 
 /**
- * 
+ *
  */
-public class VerificationResult
-{
+public class VerificationResult {
     private List<File> existenceFailures = new ArrayList<>();
 
     private List<File> nonExistenceFailures = new ArrayList<>();
@@ -38,61 +55,55 @@ public class VerificationResult
     /**
      * @param file {@link File}
      */
-    public void addExistenceFailure( File file )
-    {
-        existenceFailures.add( file );
+    public void addExistenceFailure(File file) {
+        existenceFailures.add(file);
     }
 
     /**
      * Added non existence failure.
-     * 
+     *
      * @param file {@link File}
      */
-    public void addNonExistenceFailure( File file )
-    {
-        nonExistenceFailures.add( file );
+    public void addNonExistenceFailure(File file) {
+        nonExistenceFailures.add(file);
     }
 
     /**
      * Add content failure.
-     * 
+     *
      * @param file {@link File}
      */
-    public void addContentFailure( File file )
-    {
-        contentFailures.add( file );
+    public void addContentFailure(File file) {
+        contentFailures.add(file);
     }
 
     /**
      * @return {@link #existenceFailures}
      */
-    public List<File> getExistenceFailures()
-    {
+    public List<File> getExistenceFailures() {
         return existenceFailures;
     }
 
     /**
      * @return {@link #nonExistenceFailures}
      */
-    public List<File> getNonExistenceFailures()
-    {
+    public List<File> getNonExistenceFailures() {
         return nonExistenceFailures;
     }
 
     /**
      * @return {@link #contentFailures}
      */
-    public List<File> getContentFailures()
-    {
+    public List<File> getContentFailures() {
         return contentFailures;
     }
 
     /**
      * @return true if a failures exists false otherwise.
      */
-    public boolean hasFailures()
-    {
-        return !getExistenceFailures().isEmpty() || !getNonExistenceFailures().isEmpty()
-            || !getContentFailures().isEmpty();
+    public boolean hasFailures() {
+        return !getExistenceFailures().isEmpty()
+                || !getNonExistenceFailures().isEmpty()
+                || !getContentFailures().isEmpty();
     }
 }
--- src/test/java/org/apache/maven/plugins/verifier/VerifierMojoTest.java
+++ src/test/java/org/apache/maven/plugins/verifier/VerifierMojoTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugins.verifier;
 
 /*
@@ -19,201 +37,164 @@ package org.apache.maven.plugins.verifier;
  * under the License.
  */
 
-import org.apache.maven.plugin.MojoExecutionException;
-import org.junit.Test;
-
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
+import org.apache.maven.plugin.MojoExecutionException;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class VerifierMojoTest
-{
-    private File getResourceFile( String name ) throws UnsupportedEncodingException
-    {
-        String file = getClass().getResource( name ).getFile();
-        String decode = URLDecoder.decode( file, StandardCharsets.UTF_8.toString()  ); // necessary for JDK 1.5+, where spaces are escaped to %20
-        return new File( decode );
+public class VerifierMojoTest {
+    private File getResourceFile(String name) throws UnsupportedEncodingException {
+        String file = getClass().getResource(name).getFile();
+        String decode = URLDecoder.decode(
+                file, StandardCharsets.UTF_8.toString()); // necessary for JDK 1.5+, where spaces are escaped to %20
+        return new File(decode);
     }
 
     @Test
-    public void testPrefixWithBaseDir()
-    {
+    public void testPrefixWithBaseDir() {
         VerifierMojo mojo = new VerifierMojo();
-        mojo.setBaseDir( new File( "c:/some/path" ) );
+        mojo.setBaseDir(new File("c:/some/path"));
 
-        File result = mojo.getAbsoluteFileToCheck( new File( "target/dummy.txt" ) );
+        File result = mojo.getAbsoluteFileToCheck(new File("target/dummy.txt"));
 
-        File expectedResult = new File( "c:/some/path/target/dummy.txt" );
-        assertEquals( expectedResult.getPath(), result.getPath() );
+        File expectedResult = new File("c:/some/path/target/dummy.txt");
+        assertEquals(expectedResult.getPath(), result.getPath());
     }
 
     @Test
-    public void testDoNotPrefixWhenAbsolutePath()
-    {
+    public void testDoNotPrefixWhenAbsolutePath() {
         VerifierMojo mojo = new VerifierMojo();
-        mojo.setBaseDir( new File( "/some/path" ).getAbsoluteFile() );
+        mojo.setBaseDir(new File("/some/path").getAbsoluteFile());
 
-        File absoluteFile = new File( "/project/target/dummy.txt" ).getAbsoluteFile();
-        File result = mojo.getAbsoluteFileToCheck( absoluteFile );
+        File absoluteFile = new File("/project/target/dummy.txt").getAbsoluteFile();
+        File result = mojo.getAbsoluteFileToCheck(absoluteFile);
 
-        assertEquals( absoluteFile.getPath(), result.getPath() );
+        assertEquals(absoluteFile.getPath(), result.getPath());
     }
 
     @Test
-    public void testCheckFileThatDoesNotExist()
-        throws Exception
-    {
+    public void testCheckFileThatDoesNotExist() throws Exception {
         VerifierMojo mojo = new VerifierMojo();
-        File file = getResourceFile( "/FileDoesNotExist.xml" );
-        mojo.setBaseDir( new File( "c:/some/path" ) );
-        mojo.setVerificationFile( file );
-        mojo.setFailOnError( true );
-        mojo.setVerificationResultPrinter( new VerificationResultPrinter()
-        {
-            public void print( VerificationResult result )
-            {
-                assertEquals( 1, result.getExistenceFailures().size() );
-                assertEquals( 0, result.getNonExistenceFailures().size() );
-                assertEquals( 0, result.getContentFailures().size() );
+        File file = getResourceFile("/FileDoesNotExist.xml");
+        mojo.setBaseDir(new File("c:/some/path"));
+        mojo.setVerificationFile(file);
+        mojo.setFailOnError(true);
+        mojo.setVerificationResultPrinter(new VerificationResultPrinter() {
+            public void print(VerificationResult result) {
+                assertEquals(1, result.getExistenceFailures().size());
+                assertEquals(0, result.getNonExistenceFailures().size());
+                assertEquals(0, result.getContentFailures().size());
             }
-        } );
+        });
 
-        try
-        {
+        try {
             mojo.execute();
-            fail( "Should have thrown an exception" );
-        }
-        catch ( MojoExecutionException expected )
-        {
-            assertTrue( true );
+            fail("Should have thrown an exception");
+        } catch (MojoExecutionException expected) {
+            assertTrue(true);
         }
     }
 
     @Test
-    public void testCheckFileThatExists()
-        throws Exception
-    {
+    public void testCheckFileThatExists() throws Exception {
         VerifierMojo mojo = new VerifierMojo();
-        File file = getResourceFile( "/File Exists.xml" );
-        mojo.setBaseDir( file.getParentFile() );
-        mojo.setVerificationFile( file );
-        mojo.setFailOnError( true );
-        mojo.setVerificationResultPrinter( new VerificationResultPrinter()
-        {
-            public void print( VerificationResult result )
-            {
-                assertEquals( 0, result.getExistenceFailures().size() );
-                assertEquals( 0, result.getNonExistenceFailures().size() );
-                assertEquals( 0, result.getContentFailures().size() );
+        File file = getResourceFile("/File Exists.xml");
+        mojo.setBaseDir(file.getParentFile());
+        mojo.setVerificationFile(file);
+        mojo.setFailOnError(true);
+        mojo.setVerificationResultPrinter(new VerificationResultPrinter() {
+            public void print(VerificationResult result) {
+                assertEquals(0, result.getExistenceFailures().size());
+                assertEquals(0, result.getNonExistenceFailures().size());
+                assertEquals(0, result.getContentFailures().size());
             }
-        } );
+        });
 
         mojo.execute();
     }
 
     @Test
-    public void testCheckForInexistentFile()
-        throws Exception
-    {
+    public void testCheckForInexistentFile() throws Exception {
         VerifierMojo mojo = new VerifierMojo();
-        File file = getResourceFile( "/InexistentFile.xml" );
-        mojo.setBaseDir( new File( "c:/some/path" ) );
-        mojo.setVerificationFile( file );
-        mojo.setVerificationResultPrinter( new VerificationResultPrinter()
-        {
-            public void print( VerificationResult result )
-            {
-                assertEquals( 0, result.getExistenceFailures().size() );
-                assertEquals( 0, result.getNonExistenceFailures().size() );
-                assertEquals( 0, result.getContentFailures().size() );
+        File file = getResourceFile("/InexistentFile.xml");
+        mojo.setBaseDir(new File("c:/some/path"));
+        mojo.setVerificationFile(file);
+        mojo.setVerificationResultPrinter(new VerificationResultPrinter() {
+            public void print(VerificationResult result) {
+                assertEquals(0, result.getExistenceFailures().size());
+                assertEquals(0, result.getNonExistenceFailures().size());
+                assertEquals(0, result.getContentFailures().size());
             }
-        } );
+        });
 
         mojo.execute();
     }
 
     @Test
-    public void testCheckForInexistentFileThatExists()
-        throws Exception
-    {
+    public void testCheckForInexistentFileThatExists() throws Exception {
         VerifierMojo mojo = new VerifierMojo();
-        File file = getResourceFile( "/InexistentFileThatExists.xml" );
-        mojo.setBaseDir( file.getParentFile() );
-        mojo.setVerificationFile( file );
-        mojo.setFailOnError( true );
-        mojo.setVerificationResultPrinter( new VerificationResultPrinter()
-        {
-            public void print( VerificationResult result )
-            {
-                assertEquals( 0, result.getExistenceFailures().size() );
-                assertEquals( 1, result.getNonExistenceFailures().size() );
-                assertEquals( 0, result.getContentFailures().size() );
+        File file = getResourceFile("/InexistentFileThatExists.xml");
+        mojo.setBaseDir(file.getParentFile());
+        mojo.setVerificationFile(file);
+        mojo.setFailOnError(true);
+        mojo.setVerificationResultPrinter(new VerificationResultPrinter() {
+            public void print(VerificationResult result) {
+                assertEquals(0, result.getExistenceFailures().size());
+                assertEquals(1, result.getNonExistenceFailures().size());
+                assertEquals(0, result.getContentFailures().size());
             }
-        } );
+        });
 
-        try
-        {
+        try {
             mojo.execute();
-            fail( "Should have thrown an exception" );
-        }
-        catch ( MojoExecutionException expected )
-        {
-            assertTrue( true );
+            fail("Should have thrown an exception");
+        } catch (MojoExecutionException expected) {
+            assertTrue(true);
         }
     }
 
     @Test
-    public void testCheckFileForContent()
-        throws Exception
-    {
+    public void testCheckFileForContent() throws Exception {
         VerifierMojo mojo = new VerifierMojo();
-        File file = getResourceFile( "/FileExistsValidContent.xml" );
-        mojo.setBaseDir( file.getParentFile() );
-        mojo.setVerificationFile( file );
-        mojo.setVerificationResultPrinter( new VerificationResultPrinter()
-        {
-            public void print( VerificationResult result )
-            {
-                assertEquals( 0, result.getExistenceFailures().size() );
-                assertEquals( 0, result.getNonExistenceFailures().size() );
-                assertEquals( 0, result.getContentFailures().size() );
+        File file = getResourceFile("/FileExistsValidContent.xml");
+        mojo.setBaseDir(file.getParentFile());
+        mojo.setVerificationFile(file);
+        mojo.setVerificationResultPrinter(new VerificationResultPrinter() {
+            public void print(VerificationResult result) {
+                assertEquals(0, result.getExistenceFailures().size());
+                assertEquals(0, result.getNonExistenceFailures().size());
+                assertEquals(0, result.getContentFailures().size());
             }
-        } );
+        });
 
         mojo.execute();
     }
 
     @Test
-    public void testCheckFileForInvalidContent()
-        throws Exception
-    {
+    public void testCheckFileForInvalidContent() throws Exception {
         VerifierMojo mojo = new VerifierMojo();
-        File file = getResourceFile( "/FileExistsInvalidContent.xml" );
-        mojo.setBaseDir( file.getParentFile() );
-        mojo.setVerificationFile( file );
-        mojo.setFailOnError( true );
-        mojo.setVerificationResultPrinter( new VerificationResultPrinter()
-        {
-            public void print( VerificationResult result )
-            {
-                assertEquals( 0, result.getExistenceFailures().size() );
-                assertEquals( 0, result.getNonExistenceFailures().size() );
-                assertEquals( 1, result.getContentFailures().size() );
+        File file = getResourceFile("/FileExistsInvalidContent.xml");
+        mojo.setBaseDir(file.getParentFile());
+        mojo.setVerificationFile(file);
+        mojo.setFailOnError(true);
+        mojo.setVerificationResultPrinter(new VerificationResultPrinter() {
+            public void print(VerificationResult result) {
+                assertEquals(0, result.getExistenceFailures().size());
+                assertEquals(0, result.getNonExistenceFailures().size());
+                assertEquals(1, result.getContentFailures().size());
             }
-        } );
+        });
 
-        try
-        {
+        try {
             mojo.execute();
-            fail( "Should have thrown an exception" );
-        }
-        catch ( MojoExecutionException expected )
-        {
-            assertTrue( true );
+            fail("Should have thrown an exception");
+        } catch (MojoExecutionException expected) {
+            assertTrue(true);
         }
     }
-
 }
