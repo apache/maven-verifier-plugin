5d8ee0dbcfcae42538294e9d17f12c2d757d0fdd
Gerd Aschemann <gerd@aschemann.net>
2025-01-12 14:49:39 +0100

    Clean up code with `spotless:apply`
    which leads to many changes (
    Cf. support-and-care/maven-support-and-care#77)

--- src/main/java/org/apache/maven/plugins/verifier/ConsoleVerificationResultPrinter.java
+++ src/main/java/org/apache/maven/plugins/verifier/ConsoleVerificationResultPrinter.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugins.verifier;
 
 /*
@@ -23,44 +41,36 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.verifier.model.File;
 
 /**
- * 
+ *
  */
-public class ConsoleVerificationResultPrinter
-    implements VerificationResultPrinter
-{
+public class ConsoleVerificationResultPrinter implements VerificationResultPrinter {
     private Log log;
 
     /**
      * @param log {@link Log}
      */
-    public ConsoleVerificationResultPrinter( Log log )
-    {
+    public ConsoleVerificationResultPrinter(Log log) {
         this.log = log;
     }
 
     /**
      * {@inheritDoc}
      */
-    public void print( VerificationResult results )
-    {
-        for ( File file : results.getExistenceFailures() )
-        {
-            printMessage( "File not found [" + file.getLocation() + "]" );
+    public void print(VerificationResult results) {
+        for (File file : results.getExistenceFailures()) {
+            printMessage("File not found [" + file.getLocation() + "]");
         }
 
-        for ( File file : results.getNonExistenceFailures() )
-        {
-            printMessage( "File should not exist [" + file.getLocation() + "]" );
+        for (File file : results.getNonExistenceFailures()) {
+            printMessage("File should not exist [" + file.getLocation() + "]");
         }
 
-        for ( File file : results.getContentFailures() )
-        {
-            printMessage( "File [" + file.getLocation() + "] does not match regexp [" + file.getContains() + "]" );
+        for (File file : results.getContentFailures()) {
+            printMessage("File [" + file.getLocation() + "] does not match regexp [" + file.getContains() + "]");
         }
     }
 
-    private void printMessage( String message )
-    {
-        this.log.error( "[Verifier] " + message );
+    private void printMessage(String message) {
+        this.log.error("[Verifier] " + message);
     }
 }
--- src/main/java/org/apache/maven/plugins/verifier/VerifierMojo.java
+++ src/main/java/org/apache/maven/plugins/verifier/VerifierMojo.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugins.verifier;
 
 /*
@@ -25,6 +43,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -41,44 +60,41 @@ import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
  *
  * @author <a href="vmassol@apache.org">Vincent Massol</a>
  */
-@Mojo( name = "verify", defaultPhase = LifecyclePhase.INTEGRATION_TEST )
-public class VerifierMojo
-    extends AbstractMojo
-{
+@Mojo(name = "verify", defaultPhase = LifecyclePhase.INTEGRATION_TEST)
+public class VerifierMojo extends AbstractMojo {
     /**
      * Project base directory (prepended to relative file paths).
      */
-    @Parameter( property = "basedir", required = true )
+    @Parameter(property = "basedir", required = true)
     private File basedir;
 
     /**
      * The file containing the verifications to perform.
      */
-    @Parameter( property = "verifier.verificationFile", defaultValue = "${basedir}/src/test/verifier/verifications.xml",
-            required = true )
+    @Parameter(
+            property = "verifier.verificationFile",
+            defaultValue = "${basedir}/src/test/verifier/verifications.xml",
+            required = true)
     private File verificationFile;
 
     /**
      * Whether the build will fail on verification errors.
      */
-    @Parameter( property = "verifier.failOnError", defaultValue = "true", required = true )
+    @Parameter(property = "verifier.failOnError", defaultValue = "true", required = true)
     private boolean failOnError;
 
-    private VerificationResultPrinter resultPrinter = new ConsoleVerificationResultPrinter( getLog() );
+    private VerificationResultPrinter resultPrinter = new ConsoleVerificationResultPrinter(getLog());
 
     /**
      * {@inheritDoc}
      */
-    public void execute()
-        throws MojoExecutionException
-    {
+    public void execute() throws MojoExecutionException {
         VerificationResult results = verify();
-        resultPrinter.print( results );
+        resultPrinter.print(results);
 
         // Fail the build if there are errors
-        if ( this.failOnError && results.hasFailures() )
-        {
-            throw new MojoExecutionException( "There are test failures" );
+        if (this.failOnError && results.hasFailures()) {
+            throw new MojoExecutionException("There are test failures");
         }
     }
 
@@ -87,111 +103,88 @@ public class VerifierMojo
      *            the project's basedir will be prefixed.
      * @return the absolute file path of the file to check
      */
-    protected File getAbsoluteFileToCheck( File file )
-    {
+    protected File getAbsoluteFileToCheck(File file) {
         File result = file;
-        if ( !file.isAbsolute() )
-        {
-            result = new File( basedir, file.getPath() );
+        if (!file.isAbsolute()) {
+            result = new File(basedir, file.getPath());
         }
         return result;
     }
 
-    private VerificationResult verify()
-        throws MojoExecutionException
-    {
+    private VerificationResult verify() throws MojoExecutionException {
         VerificationResult results = new VerificationResult();
 
-        try ( Reader reader = new FileReader( verificationFile ) )
-        {
+        try (Reader reader = new FileReader(verificationFile)) {
             VerificationsXpp3Reader xppReader = new VerificationsXpp3Reader();
-            Verifications verifications = xppReader.read( reader );
+            Verifications verifications = xppReader.read(reader);
 
-            for ( org.apache.maven.plugins.verifier.model.File file : verifications.getFiles() )
-            {
+            for (org.apache.maven.plugins.verifier.model.File file : verifications.getFiles()) {
                 // Transform the file to check into an absolute path prefixing the basedir if
                 // the location is relative
-                if ( file.getLocation() != null )
-                {
-                    file.setLocation( getAbsoluteFileToCheck( new File( file.getLocation() ) ).getPath() );
-                    verifyFile( file, results );
-                }
-                else
-                {
-                    throw new MojoExecutionException( "Missing <location> element" );
+                if (file.getLocation() != null) {
+                    file.setLocation(
+                            getAbsoluteFileToCheck(new File(file.getLocation())).getPath());
+                    verifyFile(file, results);
+                } else {
+                    throw new MojoExecutionException("Missing <location> element");
                 }
             }
-        }
-        catch ( XmlPullParserException | IOException e )
-        {
-            throw new MojoExecutionException( "Error while verifying files", e );
+        } catch (XmlPullParserException | IOException e) {
+            throw new MojoExecutionException("Error while verifying files", e);
         }
 
         return results;
     }
 
-    private boolean verifyFile( org.apache.maven.plugins.verifier.model.File fileCheck, VerificationResult results )
-        throws IOException
-    {
+    private boolean verifyFile(org.apache.maven.plugins.verifier.model.File fileCheck, VerificationResult results)
+            throws IOException {
         boolean result;
 
-        result = verifyFileExistence( fileCheck, results );
-        if ( result && fileCheck.getContains() != null )
-        {
-            result = result && verifyFileContent( fileCheck, results );
+        result = verifyFileExistence(fileCheck, results);
+        if (result && fileCheck.getContains() != null) {
+            result = result && verifyFileContent(fileCheck, results);
         }
 
         return result;
     }
 
-    private boolean verifyFileContent( org.apache.maven.plugins.verifier.model.File fileCheck,
-            VerificationResult results )
-        throws IOException
-    {
+    private boolean verifyFileContent(
+            org.apache.maven.plugins.verifier.model.File fileCheck, VerificationResult results) throws IOException {
         boolean result = false;
 
-        getLog().debug( "Verifying contents of " + fileCheck.getLocation() );
+        getLog().debug("Verifying contents of " + fileCheck.getLocation());
 
-        Pattern pattern = Pattern.compile( fileCheck.getContains() );
+        Pattern pattern = Pattern.compile(fileCheck.getContains());
 
         // Note: Very inefficient way as we load the whole file in memory. If you have a better
         // idea, please submit it!
-        Matcher matcher = pattern.matcher( FileUtils.fileRead( new File( fileCheck.getLocation() ) ) );
+        Matcher matcher = pattern.matcher(FileUtils.fileRead(new File(fileCheck.getLocation())));
 
-        if ( matcher.find() )
-        {
+        if (matcher.find()) {
             result = true;
-        }
-        else
-        {
-            results.addContentFailure( fileCheck );
+        } else {
+            results.addContentFailure(fileCheck);
         }
 
         return result;
     }
 
-    private boolean verifyFileExistence( org.apache.maven.plugins.verifier.model.File fileCheck,
-                                         VerificationResult results )
-    {
+    private boolean verifyFileExistence(
+            org.apache.maven.plugins.verifier.model.File fileCheck, VerificationResult results) {
         boolean result;
 
-        File physicalFile = new File( fileCheck.getLocation() );
-        if ( fileCheck.isExists() )
-        {
-            getLog().debug( "Verifying existence of " + physicalFile );
+        File physicalFile = new File(fileCheck.getLocation());
+        if (fileCheck.isExists()) {
+            getLog().debug("Verifying existence of " + physicalFile);
             result = physicalFile.exists();
-            if ( !result )
-            {
-                results.addExistenceFailure( fileCheck );
+            if (!result) {
+                results.addExistenceFailure(fileCheck);
             }
-        }
-        else
-        {
-            getLog().debug( "Verifying absence of " + physicalFile );
+        } else {
+            getLog().debug("Verifying absence of " + physicalFile);
             result = !physicalFile.exists();
-            if ( !result )
-            {
-                results.addNonExistenceFailure( fileCheck );
+            if (!result) {
+                results.addNonExistenceFailure(fileCheck);
             }
         }
 
@@ -201,32 +194,28 @@ public class VerifierMojo
     /**
      * @param theBasedir Set the base directory.
      */
-    public void setBaseDir( File theBasedir )
-    {
+    public void setBaseDir(File theBasedir) {
         this.basedir = theBasedir;
     }
 
     /**
      * @param file Set the file for verification.
      */
-    public void setVerificationFile( File file )
-    {
+    public void setVerificationFile(File file) {
         this.verificationFile = file;
     }
 
     /**
      * @param printer The verification result printer {@link VerificationResultPrinter}
      */
-    public void setVerificationResultPrinter( VerificationResultPrinter printer )
-    {
+    public void setVerificationResultPrinter(VerificationResultPrinter printer) {
         this.resultPrinter = printer;
     }
 
     /**
      * @param failOnError true to fail on error false otherwise.
      */
-    public void setFailOnError( boolean failOnError )
-    {
+    public void setFailOnError(boolean failOnError) {
         this.failOnError = failOnError;
     }
 }
